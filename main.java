488d705c450069eb1a4deb03666b627531b6aa03
Baruch Rothkoff <baruchiro@gmail.com>
2018-05-15 14:52:52 +0300

    Input and possion

--- main.java
+++ main.java
@@ -1,7 +1,25 @@
 /*__MenyBuganim_302490610___BaruchRothkoff_311603252*/
 
-public class main {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
+public class main {
+
+    private static Random rnd;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Enter num of wash stations: ");
+        var washStations = scanner.nextInt();
+        System.out.print("Enter car interval (approximately 1.5): ");
+        var carInterval = scanner.nextDouble();
+        System.out.print("Enter car wash interval (approximately 3): ");
+        var carWashInterval = scanner.nextDouble();
+        System.out.print("Total runtime: ");
+        var totalRuntime = getPoisson(scanner.nextDouble());
+    }
+
+    public static double getPoisson(double seconds) {
+        return Math.abs(Math.log(rnd.nextDouble()))*seconds;
     }
 }
